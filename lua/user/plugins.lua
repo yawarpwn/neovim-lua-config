c7d9bbbf5171ffc6937f17d17c224c79ec5f1273
yawarpwn <john.kazz@gmail.com>
2022-01-26 07:50:59 -0500

    colorizer, surround pluginds added

--- lua/user/plugins.lua
+++ lua/user/plugins.lua
@@ -58,6 +58,8 @@ return packer.startup(function(use)
   use "goolord/alpha-nvim"
   use "antoinemadec/FixCursorHold.nvim" -- This is needed to fix lsp doc highlight
   use "folke/which-key.nvim"
+  use "blackCauldron7/surround.nvim"
+  use "norcalli/nvim-colorizer.lua"
 
   -- Colorschemes
   -- use "lunarvim/colorschemes" -- A bunch of colorschemes you can try out
